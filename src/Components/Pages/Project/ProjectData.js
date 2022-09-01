40817e27d84aca265413b4d261f7c38aac4b4139
Shivkumar54 <mshivkumar59@gmail.com>
2022-09-01 11:17:52 +0530

    Added disney and Youtube

--- src/Components/Pages/Project/ProjectData.js
+++ src/Components/Pages/Project/ProjectData.js
@@ -7,117 +7,119 @@ import todo from "../../projectimages/todo.png"
 import zomato from "../../projectimages/zomatoLP.png"
 import Spottr from "../../projectimages/Spottr.png"
 import wynk from "../../projectimages/wynk.png"
+import youtube from "../../projectimages/youtube.png"
+import disney from "../../projectimages/disney.png"
 
 
 
 export default [
-    {
-        id:121,
-        title:"Wynk Music Clone",
-        img: wynk,
-        desc: `I have built Wynk music from scratch using React and Bootstrap. It is responsive for all the devices. I used the TMDB movie database for movie details. Deployed in vercel  `,
-        siteLink: `https://wynkmusic-clone.vercel.app/`,
-        codeLink:`https://github.com/Shivkumar54/wynkmusic-clone`,
-        
-    },
-    {
-        id:1,
-        title:"Book my Show Clone",
-        img: bms,
-        desc: `I have built Book my show clone from scratch using React and Tailwind CSS. It is responsive for all the devices. I used the TMDB movie database for movie details. I build this during my internship at Shape AI `,
-        siteLink: `https://book-my-show-clone-taupe.vercel.app/`,
-        codeLink:`https://github.com/Shivkumar54/Book_My_Show_Clone`,
-        
-    },
-    {
-        id:2,
-        title:"Spottr",
-        img: Spottr,
-        desc: `I have built Spottr website which is an online student PG finder website I have designed using Adobe XD and developed using React JS and this is responsive for all the devices and deployed in vercel`,
-        
-        siteLink: `https://spottr-pg-finder.vercel.app/`,
-        codeLink:`https://github.com/Shivkumar54/spottr`,
-        
-    },
-    {
-        id:3,
-        title:"Meme Generator",
-        img: meme,
-        desc: `I have built a dynamic page application called “Meme Generator” 
+  {
+    id: 1,
+    title: "Book my Show Clone",
+    img: bms,
+    desc: `I have built Book my show clone from scratch using React and Tailwind CSS. It is responsive for all the devices. I used the TMDB movie database for movie details. I build this during my internship at Shape AI `,
+    siteLink: `https://book-my-show-clone-taupe.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/Book_My_Show_Clone`,
+  },
+  {
+    id: 12174687,
+    title: "Disney+ Hotstar - Clone",
+    img: disney,
+    desc: `I have built Disney+ Hotstar from scratch using React and Bootstrap. It is responsive for all the devices. I used the TMDB movie database for movie details. Deployed in vercel  `,
+    siteLink: `https://hotstar-clone-beta.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/hotstarClone`,
+  },
+  {
+    id: 121,
+    title: "Wynk Music Clone",
+    img: wynk,
+    desc: `I have built Wynk music from scratch using React and Bootstrap. It is responsive for all the devices. I used the TMDB movie database for movie details. Deployed in vercel  `,
+    siteLink: `https://wynkmusic-clone.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/wynkmusic-clone`,
+  },
+  {
+    id: 12145,
+    title: "YouTube - Clone",
+    img: youtube,
+    desc: `I have built YouTube from scratch using React and Bootstrap. It is responsive for all the devices. I used the TMDB movie database for movie details. Deployed in vercel  `,
+    siteLink: `https://youtube-clone-iota-one.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/youtube-clone`,
+  },
+  {
+    id: 2,
+    title: "Spottr",
+    img: Spottr,
+    desc: `I have built Spottr website which is an online student PG finder website I have designed using Adobe XD and developed using React JS and this is responsive for all the devices and deployed in vercel`,
+
+    siteLink: `https://spottr-pg-finder.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/spottr`,
+  },
+  {
+    id: 3,
+    title: "Meme Generator",
+    img: meme,
+    desc: `I have built a dynamic page application called “Meme Generator” 
         which is a fun app where you can generate a random meme by clicking "getRandom Meme Button". And add your own text. `,
-        siteLink: `https://meme-generator-six-xi.vercel.app/`,
-        codeLink:`https://github.com/Shivkumar54/meme_generator`,
-        
-    },
-    {
-        id:4,
-        title:"Taskie ",
-        img: todo,
-        desc: `It is a Task manager Application  in which users can store their day to day tasks in this application. 
+    siteLink: `https://meme-generator-six-xi.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/meme_generator`,
+  },
+  {
+    id: 4,
+    title: "Taskie ",
+    img: todo,
+    desc: `It is a Task manager Application  in which users can store their day to day tasks in this application. 
         I build this during my internship at Shape AI`,
-        siteLink: `https://taskify-project-02.vercel.app/`,
-        codeLink:`https://github.com/Shivkumar54/Taskify_project_02`,
-        
-    },
-    {
-        id:5,
-        title:"E- Commerce",
-        img: ecommerce,
-        desc: `I have built E Commerce site from scratch using HTML CSS and JavaScript. It is responsive for all the devices. I build this by a YouTube tutorial`,
-        siteLink: `https://ecommerce-site-self.vercel.app/`,
-        codeLink:`https://github.com/Shivkumar54/ecommerce`,
-        
-    },
-    {
-        id:6,
-        title:"Tenzie",
-        img: todo,
-        desc: ` 
+    siteLink: `https://taskify-project-02.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/Taskify_project_02`,
+  },
+  {
+    id: 5,
+    title: "E- Commerce",
+    img: ecommerce,
+    desc: `I have built E Commerce site from scratch using HTML CSS and JavaScript. It is responsive for all the devices. I build this by a YouTube tutorial`,
+    siteLink: `https://ecommerce-site-self.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/ecommerce`,
+  },
+  {
+    id: 6,
+    title: "Tenzie",
+    img: todo,
+    desc: ` 
         Here's how to play: be the first player 
         to roll all ten of your dice to the same number.
         Please Open in Desktop mode in mobile devices  `,
-        siteLink: `https://tenzie-game-ruby.vercel.app/`,
-        codeLink:`https://github.com/Shivkumar54/tenzie `,
-        
-    },
-    {
-        id:7,
-        title:"Movie Updater",
-        img: movie,
-        desc: `Built using React Js and styled in CSS. 
+    siteLink: `https://tenzie-game-ruby.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/tenzie `,
+  },
+  {
+    id: 7,
+    title: "Movie Updater",
+    img: movie,
+    desc: `Built using React Js and styled in CSS. 
         It is a simple page where you find Movie Updates . 
         It is responsive for all the devices and deployed in vercel`,
-        siteLink: `https://movie-updater.vercel.app/`,
-        codeLink:`https://github.com/Shivkumar54/MovieUpdater`,
-        
-    },
-    {
-        id:8,
-        title:"BackUp Login Page",
-        img: backup,
-        desc: `Built in HTML and CSS. 
+    siteLink: `https://movie-updater.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/MovieUpdater`,
+  },
+  {
+    id: 8,
+    title: "BackUp Login Page",
+    img: backup,
+    desc: `Built in HTML and CSS. 
         It is a simple Login page called Backup login Page. 
         It is responsive for all the devices and deployed in vercel  `,
-        siteLink: `https://loginpage-backup.vercel.app/`,
-        codeLink: `https://github.com/Shivkumar54/loginpage_backup`,
-        
-    },
-  
-    {
-        id:9,
-        title:"Zomato Landing Page",
-        img: zomato,
-        desc: `Built using HTML5 styled in CSS. 
+    siteLink: `https://loginpage-backup.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/loginpage_backup`,
+  },
+
+  {
+    id: 9,
+    title: "Zomato Landing Page",
+    img: zomato,
+    desc: `Built using HTML5 styled in CSS. 
         It is a simple page Zomato landing page . 
         It is responsive for all the devices and deployed in vercel   `,
-        siteLink: `https://my-zomato-project-001.vercel.app/`,
-        codeLink: `https://github.com/Shivkumar54/Zomato_front_end`,
-        
-    },
-  
-    
-
-
-
-
+    siteLink: `https://my-zomato-project-001.vercel.app/`,
+    codeLink: `https://github.com/Shivkumar54/Zomato_front_end`,
+  },
 ]
--- src/Components/Pages/Project/Projects.js
+++ src/Components/Pages/Project/Projects.js
@@ -1,71 +1,73 @@
-import React, {useState} from 'react'
+import React, { useState } from "react"
 import projectData from "./ProjectData"
 import ProjectImg from "../../images/projects.png"
 import "./project.css"
 
 const Projects = () => {
-
-
   const [items, setItems] = useState(projectData)
 
   const CardList = items.map((eachItems) => {
-    const { title, img, desc, siteLink, codeLink  } = eachItems;
+    const { title, img, desc, siteLink, codeLink } = eachItems
 
-              return(
-                <div className='item1 col-12 col-md-6 col-lg-3 my-3' >
-                <div className='projectWrapper' > 
-                  {/* for Image  */}
-                    <div className='RootImage' >
-                    <img src ={img} alt="Menu Pic" className='img-fluid' />
-                  </div>
-                  {/* for Image  */}
-                  {/* for Menu Description  */}
-                  <div >
-                      <div className='texter main-title pt-4 pb-3'>
-                        <div className="d-flex justify-content-between">
-                          <h1 className='projH' >{title}</h1>
-                        </div>
-                        <p>{desc}</p>   
-                        
-                        <div className="btn-wrapr ">
-                          <a href={siteLink} className="primary alinker" >View Site</a>
-                          <a href={codeLink} className="secondary alinker" >View Code</a>
-                        </div>
-                        
-
-                      </div>
-                      
-                  </div>
-                  {/* for Menu Description  */}
-                </div>
+    return (
+      <div className="item1 col-12 col-md-6 col-lg-3 my-3">
+        <div className="projectWrapper">
+          {/* for Image  */}
+          <div className="RootImage">
+            <img src={img} alt="Menu Pic" className="img-fluid" />
+          </div>
+          {/* for Image  */}
+          {/* for Menu Description  */}
+          <div>
+            <div className="texter main-title pt-4 pb-3">
+              <div className="d-flex justify-content-between">
+                <h1 className="eachProjects">{title}</h1>
               </div>
-              )
-            })
+              <p>{desc}</p>
+
+              <div className="btn-wrapr ">
+                <a href={siteLink} className="primary alinker">
+                  View Site
+                </a>
+                <a href={codeLink} className="secondary alinker">
+                  View Code
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* for Menu Description  */}
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
-      <div className='container projRoot ' >
+      <div className="container projRoot ">
         <div className="flexerImg">
           <img src={ProjectImg} alt="" />
         </div>
         <div className="flexertxt">
-          <h1 className='aboutSubText cont projH1 ' >Projects & Accomplishment</h1>
-          <p className='projDesc' >These are some of my projects and accomplishments, some project I did myself and some are from my "Internships" and from YouTube tutorials. Do Visit the site or View the code by clicking the buttons given below the project and leave your valuable feedbacks in contact page. <br /> Thankyou </p>
-
-
+          <h1 className="aboutSubText cont projHeading ">
+            Projects & Accomplishment
+          </h1>
+          <p className="projDesc">
+            These are some of my projects and accomplishments, some project I
+            did myself and some are from my "Internships" and from YouTube
+            tutorials. Do Visit the site or View the code by clicking the
+            buttons given below the project and leave your valuable feedbacks in
+            contact page. <br /> Thankyou{" "}
+          </p>
         </div>
       </div>
 
-    <div className='menu-items container'>
-      <div className='row'>
-        <div className='col-12 mx-auto'>
-          <div className='row my-4'>     
-            {CardList}
+      <div className="menu-items container">
+        <div className="row">
+          <div className="col-12 mx-auto">
+            <div className="row my-4">{CardList}</div>
           </div>
         </div>
       </div>
-      
-   </div>
     </div>
   )
 }
